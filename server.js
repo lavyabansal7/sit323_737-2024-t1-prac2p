a53c393267d65add6b9e83bad9c3676db6e0cf29
lavyabansal7 <104566086+lavyabansal7@users.noreply.github.com>
2024-03-18 17:32:43 +1100

    Task 2.1P

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// picks files from public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// to start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
